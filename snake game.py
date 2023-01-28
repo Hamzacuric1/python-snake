c866bcf8c6f5315fec6ffb282a1e1432399da54f
Hamzacuric1 <hamzacuric47@gmail.com>
2023-01-28 14:10:05 +0100

    adding snake head

--- snake game.py
+++ snake game.py
@@ -1,7 +1,48 @@
-from turtle import *
-wn = Screen()
-wn.title('Snake Game')
-wn.bgcolor('green')
-wn.setup(width=600, height=600)
-wn.tracer(0)
-wn.mainloop()
+import turtle
+import time
+
+delay = 0.1
+#pozadina
+sg = turtle.Screen()
+sg.title('Snake Game by Hamza Curic')
+sg.bgcolor('green')
+sg.setup(width=600, height=600)
+sg.tracer(0)
+sg.mainloop()
+
+#glava
+head = turtle.Turtle()
+head.speed(0)
+head.shape('square')
+head.color('black')
+head.penup()
+head.goto(0,0)
+head.direction = 'up'
+
+#Fuctions
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+
+    if head.direction == "left":
+        x = head.xcor()
+        head.sety(y + 20)
+
+    if head.direction == "right":
+        x = head.xcor()
+        head.sety(y + 20)
+
+#Main game loop
+while True:
+    sg.update()
+
+    move()
+
+    time.sleep(delay)
+
+sg.mainloop()
